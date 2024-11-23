6a433a0cb50d4e84b5a40651115215fdc6e0a83a
ElBoyTM <nszalenski@gmail.com>
2024-11-23 16:09:54 -0500

    filled out auth-routes, 1st draft

--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,23 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { id, password } = req.body;
+  const user = await User.findOne({ where: { id } });
+  if (!user) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    res.status(401).send('Invalid password');
+    return;
+  }
+  
+  const token = jwt.sign({ id }, process.env.JWT_SECRET || 'default_secret', {
+    expiresIn: '1h',
+  });
+  res.send(token);
 };
 
 const router = Router();
